d8cbbb2b05619982430671c6a9ac61654a05f188
sagar-trinity <gsagar@trinitylifesciences.com>
2025-05-12 08:04:36 +0530

    Update langgraph_email_bot.py

--- langgraph_email_bot.py
+++ langgraph_email_bot.py
@@ -17,18 +17,18 @@ import os
 
 embeddings = AzureOpenAIEmbeddings(
     model=os.getenv("AZURE_OPENAI_API_EMBEDDING_MODEL"),
-    azure_deployment=AZURE_OPENAI_API_EMBEDDING_DEPLOYMENT_NAME,
-    api_key=AZURE_OPENAI_KEY,
-    openai_api_version=OPENAI_API_VERSION,
-    azure_endpoint=AZURE_OPENAI_ENDPOINT,
-    openai_api_type=OPENAI_API_TYPE
+    azure_deployment=os.getenv("AZURE_OPENAI_API_EMBEDDING_DEPLOYMENT_NAME"),
+    api_key=os.getenv("AZURE_OPENAI_KEY"),
+    openai_api_version=os.getenv("OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    openai_api_type=os.getenv("OPENAI_API_TYPE")
 )
 
-openai.api_type = constant.OPENAI_API_TYPE
-openai.api_version = constant.OPENAI_API_VERSION
-openai.api_key = constant.AZURE_OPENAI_KEY
-openai.api_base = constant.OPENAI_API_BASE
-llm = AzureChatOpenAI(deployment_name=constant.AZURE_OPENAI_DEPLOYMENT_NAME, temperature=0, cache=False, openai_api_key=openai.api_key, openai_api_version=openai.api_version)
+openai.api_type = os.getenv("OPENAI_API_TYPE")
+openai.api_version = os.getenv("OPENAI_API_VERSION")
+openai.api_key = os.getenv("AZURE_OPENAI_KEY")
+openai.api_base = os.getenv("OPENAI_API_BASE")
+llm = AzureChatOpenAI(deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"), temperature=0, cache=False, openai_api_key=openai.api_key, openai_api_version=openai.api_version)
 
 @tool
 def mail(to: str, subject: str, body: str):
